3054bdd38143416dfe7398fbed7492c2594088b1
Bruna Flôr <brn.flor@gmail.com>
2022-04-10 14:03:30 +0100

    structure

---
+++ scripts/plotTable.js
@@ -0,0 +1,32 @@
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const words = ["ban", "grup"];
+
+const table = {
+  //line:column
+  line1: ["x", "B", "C"],
+  line2: ["b", "a", "n"],
+  line3: ["G", "H", "I"],
+};
+
+const container = document.getElementById("wordHuntContainer");
+
+const processedTable = Object.keys(table);
+
+processedTable.forEach((line, lIndex) => {
+  table[line].forEach((column, cIndex) => {
+
+    const cellId = `L${lIndex}_C${cIndex}`;
+    const tableCell = document.createElement("div");
+    
+    tableCell.setAttribute("class", "tableCell");
+    tableCell.setAttribute("id", cellId);
+    
+    tableCell.addEventListener("click", () => {
+      selectCell(cellId);
+    });
+
+    container.appendChild(tableCell);
+    tableCell.innerHTML = column;
+  });
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,12 @@
-const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-const words = ["ban", "grup"];
+const selectCell = (id) =>{
+    const selectedCell = document.getElementById(id)
+   if (!selectedCell.classList.contains("selected")) {
+       selectedCell.classList.add("selected")
 
-const table = {
-  //line:column
-  line1: ["x", "B", "C"],
-  line2: ["b", "a", "n"],
-  line3: ["G", "H", "I"],
-};
+   } else {
+       selectedCell.classList.remove("selected")
 
-const processedTable = Object.keys(table);
+   }
 
-//for find if the word exist in the table
-processedTable.forEach((line) => {
-  table[line].forEach((column) => {
-
-    words.forEach((word) => {
-      for (let i = 0; i < word.length; i++) {
-
-        if (column === word[i]) {
-          console.log(column);        
-        }
-        
-      
-    }
-    });
-
-  });
-});
+}
