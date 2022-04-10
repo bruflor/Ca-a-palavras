c80b02b8f25cb8fe09309b6e688c0e1c0a6b6532
Bruna Flôr <brn.flor@gmail.com>
2022-04-10 12:51:12 +0100

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const words = ["ban", "grup"];
+
+const table = {
+  //line:column
+  line1: ["x", "B", "C"],
+  line2: ["b", "a", "n"],
+  line3: ["G", "H", "I"],
+};
+
+const processedTable = Object.keys(table);
+
+//for find if the word exist in the table
+processedTable.forEach((line) => {
+  table[line].forEach((column) => {
+
+    words.forEach((word) => {
+      for (let i = 0; i < word.length; i++) {
+
+        if (column === word[i]) {
+          console.log(column);        
+        }
+        
+      
+    }
+    });
+
+  });
+});
