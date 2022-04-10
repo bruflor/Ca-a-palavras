864c3b0ef452060ce5ff9c7217dac2ed9bbb9658
Bruna Flôr <brn.flor@gmail.com>
2022-04-10 16:19:46 +0100

    version 0 complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,48 @@
-//get all words in a list
-// slipt each word in to letters
-
-//set a position in the table for the first letter of the word
-// set a position for the each other letter of the word, that means first letter position + 1, +2.. so on.
-
 const wordStartLine = 0;
 const wordStartColumn = 0;
-const huntedWords = ["BANANA", "AVOCADO", "MELON", "GRAPE"];
+const huntedWords = [
+  {
+    word: "BANANA",
+    direction: "Horizontal",
+    startLineNumber: 0,
+    startColumnNumber: 0,
+  },
 
-huntedWords.forEach((word, wordIndex) => {
-    // console.log(word.length)
-    const wordLength = word.length
-    for (let i =0; i<wordLength; i++){
-        const myCell = document.getElementById(`L${i}_C0`);
-        myCell.innerHTML=huntedWords[wordIndex][i];
+  {
+    word: "AVOCADO",
+    direction: "Vertical",
+    startLineNumber: 5,
+    startColumnNumber: 9,
+  },
+];
+
+huntedWords.forEach((wordObject) => {
+  const wordLength = wordObject.word.length;
+  if (wordObject.direction === "Horizontal") {
+    for (let i = 0; i < wordLength; i++) {
+      const myCell = document.getElementById(
+        `L${wordObject.startLineNumber}_C${wordObject.startColumnNumber + i}`
+      );
+      myCell.innerHTML = wordObject.word[i];
+      console.log(myCell);
     }
-})
+  } else if (wordObject.direction === "Vertical") {
+    for (let i = 0; i < wordLength; i++) {
+      const myCell = document.getElementById(
+        `L${wordObject.startLineNumber + i}_C${wordObject.startColumnNumber}`
+      );
+      myCell.innerHTML = wordObject.word[i];
+      console.log(myCell);
+    }
+  } else {
+    for (let i = 0; i < wordLength; i++) {
+      const myCell = document.getElementById(
+        `L${wordObject.startLineNumber + i}_C${wordObject.startColumnNumber+i}`
+      );
+      myCell.innerHTML = wordObject.word[i];
+      console.log(myCell);
+    }
+  }
+});
 
-// table[`line${wordStartLine}`][wordStartColumn]="H"
-// console.log(table[`line${wordStartLine}`][wordStartColumn])
-
-// const myCell1 = document.getElementById("L0_C0");
-// myCell1.innerHTML=huntedWords[0][0];
 
